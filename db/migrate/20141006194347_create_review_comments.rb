babce5c7e13aea34e96831378476b00f6e7290bf
Paul Brescia <brescia717@gmail.com>
2014-10-08 15:55:58 -0400

    Removed reviews and review_comments tables

--- db/migrate/20141006194347_create_review_comments.rb
+++
@@ -1,9 +0,0 @@
-class CreateReviewComments < ActiveRecord::Migration
-  def change
-    create_table :review_comments do |t|
-      t.text :body, null: false
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -34,25 +34,6 @@ ActiveRecord::Schema.define(version: 20141008175114) do
     t.integer  "user_id"
   end
 
-  create_table "review_comments", force: true do |t|
-    t.text     "body",       null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "review_id"
-    t.integer  "user_id"
-  end
-
-  create_table "reviews", force: true do |t|
-    t.string   "title",                  null: false
-    t.text     "body",                   null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "user_id"
-    t.integer  "build_id"
-    t.integer  "rating"
-    t.integer  "upvotes",    default: 0
-  end
-
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -64,6 +45,8 @@ ActiveRecord::Schema.define(version: 20141008175114) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "username"
   end
 
