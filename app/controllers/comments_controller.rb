556a9a5a886648d3e79b1249dd89cd19e6138f0b
Paul Brescia <brescia717@gmail.com>
2015-12-11 22:46:28 -0500

    Refactored bios, comments, and users controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+
   def new
     @comment = Comment.new
   end
@@ -6,10 +7,9 @@ class CommentsController < ApplicationController
   def create
     @team = Team.find(params[:team_id])
     @comment = Comment.new(comment_params)
-    @comment.user = current_user # these had _id and .id
+    @comment.user = current_user
     @comment.team_id = params[:team_id]
     if @comment.save
-      # CommentConfirmation.notification(@comment, @comment.team.user).deliver
       flash[:success] = "You have successfully posted your comment."
       redirect_to team_path(@team)
     else
@@ -24,8 +24,6 @@ class CommentsController < ApplicationController
 
   def update
     @comment = Comment.find(params[:id])
-    # @team = Team.find(params[:id])
-    # @comment.team_id = params[:team_id]
 
     if @comment.update(comment_params)
       flash[:notice] = "You have successfully updated your comment."
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!, only: [:upvote, :downvote]
-  require 'httparty'
+  before_action :get_user, except: [:index]
 
   def index
   end
 
   def show
-    @user  = User.find(params[:id])
     @stat  = Stat.new
     @stats = @user.stat
     @bios  = @user.bios
@@ -27,7 +26,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
 
     if @user.update(user_params)
       flash[:success] = "You have successfully updated your profile."
@@ -38,13 +36,11 @@ class UsersController < ApplicationController
   end
 
   def upvote
-    @user = User.find(params[:id])
     @user.vote_by voter: current_user, vote: 'like'
     redirect_to @user
   end
 
   def downvote
-    @user = User.find(params[:id])
     @user.vote_by voter: current_user, vote: 'bad'
     redirect_to @user
   end
@@ -54,6 +50,10 @@ class UsersController < ApplicationController
   def user_params
     return { email: nil } unless params[:user]
     params.require(:user).permit(:email)
+  end
+
+  def get_user
+    @user = User.find(params[:id])
   end
 
 end
